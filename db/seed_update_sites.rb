082d3ca55fd697da6e24bf7a0d033d27feff5aa6
Hopson Gausi <hopgausi20@gmail.com>
2023-09-15 10:21:36 +0200

    Add handling of enable sites

--- db/seed_update_sites.rb
+++ db/seed_update_sites.rb
@@ -2,11 +2,13 @@ file_sites = JSON.parse(File.read(Rails.root.join('db','seed_files','sites.json'
 
 file_sites.each do |f_site|
     site = Site.where(name: f_site['name'], district: f_site['district']).take
-    if !site.nil? 
+    if !site.nil?
+        enabled = site.enabled == true ? true : false
         site.update(host_address: f_site['host_address'], site_code: f_site['site_code'], 
-            site_code_number: f_site['site_code_number'],  enabled: f_site['enabled']
+            site_code_number: f_site['site_code_number'],  enabled: enabled
         )
     else
+        f_site['enabled'] = false
         Site.create(f_site)
     end
 end
