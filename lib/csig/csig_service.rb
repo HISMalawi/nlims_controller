5f9fba38fe62232f336a7dad9e55d4383e780cb3
Hopson Gausi <hopgausi20@gmail.com>
2023-09-28 12:57:53 +0200

    convert to base 9, prepad number with zeros, concat seq with check digit

--- lib/csig/csig_service.rb
+++ lib/csig/csig_service.rb
@@ -5,4 +5,25 @@ module CsigService
   def self.generate_ids
     return ids if ids.present?
   end
+
+  # Convert a number to base 9
+  def self.convert_to_base9(number)
+    return number if number < 9
+
+    result = ''
+    while number != 0
+      result = (number % 9).to_s + result
+      number /= 9
+    end
+    result.to_i
+  end
+
+  # Prepad a number with 0s to a given length
+  def self.prepad_number_with_zeros(length = 10, number)
+    number.to_s.rjust(length, '0')
+  end
+
+  def self.concat_check_digit_with_seq_number(check_digit, seq_number)
+    (check_digit.to_s + seq_number.to_s).to_i
+  end
 end
