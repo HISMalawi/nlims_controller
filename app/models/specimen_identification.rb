793f477a7655bc09a65a9d0d8ed435f3f37d17e1
Hopson Gausi <hopgausi20@gmail.com>
2023-10-05 15:06:44 +0200

    model validations

--- app/models/specimen_identification.rb
+++ app/models/specimen_identification.rb
@@ -1,2 +1,9 @@
 class SpecimenIdentification < ApplicationRecord
+  validates :sequence_number, presence: true, uniqueness: true
+  validates :base9_equivalent, presence: true, uniqueness: true
+  validates :base9_zero_padded, presence: true, uniqueness: true
+  validates :encrypted, presence: true, uniqueness: true
+  validates :sin, presence: true, uniqueness: true
+  validates :check_digit, presence: true
+  validates :encrypted_zero_cleaned, presence: true, uniqueness: true
 end
