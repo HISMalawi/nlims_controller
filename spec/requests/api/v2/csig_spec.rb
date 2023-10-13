c19892a56f9ad19834f5e5556c416793f72050b8
Hopson Gausi <hopgausi20@gmail.com>
2023-10-13 10:11:54 +0200

    Generate ids swagger

--- spec/requests/api/v2/csig_spec.rb
+++ spec/requests/api/v2/csig_spec.rb
@@ -1,29 +1,30 @@
+# frozen_string_literal: true
+
 require 'swagger_helper'
 
+TAGS_CSIG = 'CSIG'
 RSpec.describe 'api/v2/csig', type: :request do
-
   path '/api/v2/csig/generate_ids' do
-
     post('generate_specimen_tracking_id csig') do
+      tags TAGS_CSIG
+      consumes 'application/json'
+      parameter in: :body, schema: {
+        type: :object,
+        properties: {
+          number_of_ids: { type: :integer }
+        },
+        required: ['number_of_ids']
+      }
       response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+        let(:number_of_ids) { 1 }
         run_test!
       end
     end
   end
 
   path '/api/v2/csig/distribute_ids' do
-
     post('distribute_sin csig') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -37,10 +38,8 @@ RSpec.describe 'api/v2/csig', type: :request do
   end
 
   path '/api/v2/csig/sin_used' do
-
     get('check_if_sin_is_used csig') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -54,10 +53,8 @@ RSpec.describe 'api/v2/csig', type: :request do
   end
 
   path '/api/v2/csig/use_sin' do
-
     post('use_sin csig') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -71,89 +68,8 @@ RSpec.describe 'api/v2/csig', type: :request do
   end
 
   path '/api/v2/csig' do
-
-    get('list csigs') do
+    get('list specimen tracking ids') do
       response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    post('create csig') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v2/csig/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('show csig') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    patch('update csig') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    put('update csig') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('delete csig') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
