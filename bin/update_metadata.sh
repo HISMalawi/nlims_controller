b1e57ca1a30d7a1e78194aa5eaf3500f3eb382aa
kellworks <kchidothi@outlook.com>
2024-02-27 11:12:28 +0200

    fix: add rbs test type

--- bin/update_metadata.sh
+++ bin/update_metadata.sh
@@ -3,5 +3,6 @@ rails db:migrate && (
   rake db:seed:specific\[seed_dispatcher_types.rb\] &&
   rake db:seed:specific\[seed_test_results_recepient_types.rb\] &&
   rake db:seed:specific\[seed_name_mappings.rb\] &&
-  rake db:seed:specific\[init_tracking_number.rb\]
+  rake db:seed:specific\[init_tracking_number.rb\] &&
+  rails r bin/add_test_types.rb
 )
