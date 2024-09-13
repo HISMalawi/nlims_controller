edc4a713bfe4fd64aebba8fd9a063621918ca4ae
Hopson Gausi <hopgausi20@gmail.com>
2024-09-13 08:01:22 +0200

    moe push orders to master to test model

--- app/models/speciman.rb
+++ app/models/speciman.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 
-require 'master_nlims_sync_service'
-
 # Specimen model
 class Speciman < ApplicationRecord
-  after_commit :push_order_to_master_nlims, on: %i[create update], if: :local_nlims?
-
-  private
-
-  def local_nlims?
-    Config.local_nlims?
-  end
-
-  def push_order_to_master_nlims
-    master = MasterNlimsSyncService.new
-    payload = master.build_order_payload(tracking_number)
-    OrderSyncTracker.find_or_create_by(tracking_number:)
-    response = master.push_order_to_master_nlims(payload)
-    OrderSyncTracker.find_by(tracking_number:).update(synced: true) if response
-  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,26 @@
+# frozen_string_literal: true
+
+require 'master_nlims_sync_service'
+
+# Test model
 class Test < ApplicationRecord
+  after_commit :push_order_to_master_nlims, on: %i[create], if: :local_nlims?
+
+  private
+
+  def local_nlims?
+    Config.local_nlims?
+  end
+
+  def push_order_to_master_nlims
+    master = MasterNlimsSyncService.new
+    payload = master.build_order_payload(tracking_number)
+    OrderSyncTracker.find_or_create_by(tracking_number:)
+    response = master.push_order_to_master_nlims(payload)
+    OrderSyncTracker.find_by(tracking_number:).update(synced: true) if response
+  end
+
+  def tracking_number
+    Speciman.find_by(id: Test.find(id)&.specimen_id)&.tracking_number
+  end
 end
