8ede4b09aafdfaf34b7075b4e95873913bec89b2
Hopson Gausi <hopgausi20@gmail.com>
2024-10-07 08:16:55 +0200

    Add comment to controller

--- app/controllers/api/v1/status_controller.rb
+++ app/controllers/api/v1/status_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Status controller for tracking app status
 class API::V1::StatusController < ApplicationController
   skip_before_action :authenticate_request, only: [:ping]
 
