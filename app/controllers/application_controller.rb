556533b38c5dc4600fb1fbb80edb838cc6a3ff6e
Hopson Gausi <hopgausi20@gmail.com>
2025-02-26 11:39:39 +0200

    temp disable auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,31 +2,32 @@
 
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  # before_action :authenticate_request, except: %w[
-  #   re_authenticate check_token_validity authenticate_user create_user dispatch_sample
-  # ]
+  before_action :authenticate_request, except: %w[
+    re_authenticate check_token_validity authenticate_user create_user dispatch_sample
+  ]
 
   def authenticate_request
-    token = request.headers['token']
-    status = UserService.check_token(token)
-    if token
-      return status unless status == false
+    return true
+    # token = request.headers['token']
+    # status = UserService.check_token(token)
+    # if token
+    #   return status unless status == false
 
-      response = {
-        status: 401,
-        error: true,
-        message: 'token expired',
-        data: {}
-      }
+    #   response = {
+    #     status: 401,
+    #     error: true,
+    #     message: 'token expired',
+    #     data: {}
+    #   }
 
-    else
-      response = {
-        status: 401,
-        error: true,
-        message: 'token not provided',
-        data: {}
-      }
-    end
-    render(plain: response.to_json) && return
+    # else
+    #   response = {
+    #     status: 401,
+    #     error: true,
+    #     message: 'token not provided',
+    #     data: {}
+    #   }
+    # end
+    # render(plain: response.to_json) && return
   end
 end
