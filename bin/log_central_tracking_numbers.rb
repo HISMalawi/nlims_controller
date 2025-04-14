fa6c85b0ae811fdbd73aba25b8cb5dadc69a2609
Hopson Gausi <hopgausi20@gmail.com>
2025-04-14 15:53:29 +0200

    Add cron job for logging tracking numbers and implement logging script

--- bin/log_central_tracking_numbers.rb
+++ bin/log_central_tracking_numbers.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-last_logged_tracking_number = TrackingNumberLogger.last
+if Config.local_nlims?
+  last_logged_tracking_number = TrackingNumberLogger.last
 
-chsu_tracking_number_order_id = last_logged_tracking_number&.chsu_tracking_number_order_id || 0
+  chsu_tracking_number_order_id = last_logged_tracking_number&.chsu_tracking_number_order_id || 0
 
-nlims_service = NlimsSyncUtilsService.new(nil)
-order_tracking_numbers = nlims_service.order_tracking_numbers(chsu_tracking_number_order_id, limit: 100)
-order_tracking_numbers.each do |order_tracking_number|
-  puts "Logging tracking number: #{order_tracking_number[:tracking_number]} for order ID: #{order_tracking_number[:id]}"
-  TrackingNumberLogger.create!(
-    tracking_number: order_tracking_number[:tracking_number],
-    chsu_tracking_number_order_id: order_tracking_number[:id]
-  )
+  nlims_service = NlimsSyncUtilsService.new(nil)
+  order_tracking_numbers = nlims_service.order_tracking_numbers(chsu_tracking_number_order_id, limit: 100)
+  order_tracking_numbers.each do |order_tracking_number|
+    puts "Logging tracking number: #{order_tracking_number[:tracking_number]} for order ID: #{order_tracking_number[:id]}"
+    TrackingNumberLogger.create!(
+      tracking_number: order_tracking_number[:tracking_number],
+      chsu_tracking_number_order_id: order_tracking_number[:id]
+    )
+  end
 end
