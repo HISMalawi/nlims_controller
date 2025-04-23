bc16a7ebe26f96531931dfdb71b8e68e7b4e77ea
Hopson Gausi <hopgausi20@gmail.com>
2025-04-23 11:53:28 +0200

    Measure types

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,57 @@
 # 		)
 
 # puts '-------------done----------'
+measure_types = {
+  'Numeric' => {
+    description: 'A numeric measurement type',
+    structure: {
+      type: 'ranges',
+      parameters: [
+        { name: 'Age Range', type: 'range', values: [{ min: 'number' }, { max: 'number' }] },
+        { name: 'Measure Range', type: 'range', values: [{ min: 'number' }, { max: 'number' }] },
+        { name: 'Interpretation', type: 'string', values: 'interpretation' },
+        { name: 'Sex', type: 'options', values: %w[Male Female Both] }
+      ]
+    }
+  },
+  'Free Text' => {
+    description: 'A freely entered text value',
+    structure: {
+      type: 'free_text'
+    }
+  },
+  'AlphaNumeric' => {
+    description: 'A combination of letters and numbers',
+    structure: {
+      type: 'options',
+      parameters: [
+        { name: 'value', type: 'string', values: 'value' },
+        { name: 'Interpretation', type: 'string', values: 'interpretation' }
+      ]
+    }
+  },
+  'Rich Text' => {
+    description: 'Formatted text with styling options',
+    structure: {
+      type: 'rich_text'
+    }
+  },
+  'AutoComplete' => {
+    description: 'A combination of letters and numbers',
+    structure: {
+      type: 'options',
+      parameters: [
+        { name: 'value', type: 'string', values: 'value' },
+        { name: 'Interpretation', type: 'string', values: 'interpretation' }
+      ]
+    }
+  }
+}
+
+measure_types.each do |name, attrs|
+  MeasureType.find_or_create_by(name:).update(attrs)
+end
+
 def specimen
    MlabBase.find_by_sql('SELECT * FROM specimen').each do |specimen_mlab|
      specimen = SpecimenType.find_or_create_by!(
