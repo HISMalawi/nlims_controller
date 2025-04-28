dee6498ecb7ca04aa192bf4249d0b4a23d128d75
Hopson Gausi <hopgausi20@gmail.com>
2025-04-28 09:59:40 +0200

    Implement cleanup of SyncErrorLog entries older than 24 hours

--- app/models/sync_error_log.rb
+++ app/models/sync_error_log.rb
@@ -2,4 +2,9 @@
 
 # SyncErrorLog for logging errors in syncing orders to EMR
 class SyncErrorLog < ApplicationRecord
+  after_create :clean_up_after_24_hours
+
+  def clean_up_after_24_hours
+    SyncErrorLog.where('created_at < ?', 24.hours.ago).delete_all
+  end
 end
