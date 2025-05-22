97fc50430e9b9428ab95d7a6fd8914c438a30110
Hopson Gausi <hopgausi20@gmail.com>
2025-05-22 09:18:02 +0200

    Limit order tracking numbers to 50,000 and filter by creation date in order_tracking_numbers_to_logged method

--- bin/log_central_tracking_numbers.rb
+++ bin/log_central_tracking_numbers.rb
@@ -6,10 +6,10 @@ if Config.local_nlims?
   chsu_tracking_number_order_id = last_logged_tracking_number&.chsu_tracking_number_order_id || 0
 
   nlims_service = NlimsSyncUtilsService.new(nil)
-  order_tracking_numbers = nlims_service.order_tracking_numbers(chsu_tracking_number_order_id, limit: 200_000)
+  order_tracking_numbers = nlims_service.order_tracking_numbers(chsu_tracking_number_order_id, limit: 50_000)
   order_tracking_numbers.each do |order_tracking_number|
     puts "Logging tracking number: #{order_tracking_number[:tracking_number]} for order ID: #{order_tracking_number[:id]}"
-    TrackingNumberLogger.create!(
+    TrackingNumberLogger.find_or_create_by(
       tracking_number: order_tracking_number[:tracking_number],
       chsu_tracking_number_order_id: order_tracking_number[:id]
     )
