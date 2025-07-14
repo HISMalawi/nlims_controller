7a626497052fff7ba7b6c0e1911d0041dd18eadb
Hopson Gausi <hopgausi20@gmail.com>
2025-07-14 15:20:03 +0200

    Update NLIMS version to v2.3.1 in update-nlims.sh

--- update-nlims.sh
+++ update-nlims.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SETTINGS : Set variables 
-NLIMS_VERSION="v2.3.0" # Replace with the actual version
+NLIMS_VERSION="v2.3.1" # Replace with the actual version
 NLIMS_CONTROLLER_DIR="/var/www/nlims_controller" 
 EMR_API_DIR="/var/www/EMR-API" 
 MLAB_API_DIR="/var/www/mlab_api" 
