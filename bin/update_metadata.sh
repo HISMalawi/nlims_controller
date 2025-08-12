b297dac625af73a3a0a22f4902194deec3630229
Hopson Gausi <hopgausi20@gmail.com>
2025-08-12 13:08:15 +0200

    Use bundle exec to run scripts

--- bin/update_metadata.sh
+++ bin/update_metadata.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm Gemfile.lock && bundle install --local && rails db:migrate && (
-  rake db:seed:specific\[seed_dispatcher_types.rb\] &&
-  rake db:seed:specific\[seed_test_results_recepient_types.rb\] &&
-  rake db:seed:specific\[seed_name_mappings.rb\] &&
-  rake db:seed:specific\[init_tracking_number.rb\]
+rm Gemfile.lock && bundle install --local && bundle exec rails db:migrate && (
+  bundle exec rake db:seed:specific\[seed_dispatcher_types.rb\] &&
+  bundle exec rake db:seed:specific\[seed_test_results_recepient_types.rb\] &&
+  bundle exec rake db:seed:specific\[seed_name_mappings.rb\] &&
+  bundle exec rake db:seed:specific\[init_tracking_number.rb\]
 )
-rails r bin/updater.rb && bash bin/add_cronjob.sh
+bundle exec rails r bin/updater.rb && bash bin/add_cronjob.sh
 
 # Update couch IDs for order sources
 echo "Updating order source couch IDs..."
