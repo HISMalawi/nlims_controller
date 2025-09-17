49aac266927a1177eed9ca9273b76b4945827613
Hopson Gausi <hopgausi20@gmail.com>
2025-09-17 11:39:58 +0200

    seed additional test status

--- db/seed_additional_test_statuses.rb
+++ db/seed_additional_test_statuses.rb
@@ -4,9 +4,9 @@ test_statuses = %w[test_on_repeat]
 
 test_statuses.each do |status|
   puts "Seeding additional test status: #{status}"
-  status = TestStatus.find_by(name: status)
-  puts "#{status.name} already exists, skipping..." if status.present?
-  next if status.present?
+  lab_status = TestStatus.find_by(name: status)
+  puts "#{lab_status.name} already exists, skipping..." if lab_status.present?
+  next if lab_status.present?
 
   TestStatus.find_or_create_by!(name: status)
 end
