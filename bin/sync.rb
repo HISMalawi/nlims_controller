849af690826016541e51f07a1515b379a1da4789
Hopson Gausi <hopgausi20@gmail.com>
2025-09-25 11:18:03 +0200

    sync nlims

--- bin/sync.rb
+++ bin/sync.rb
@@ -1,8 +1,37 @@
 # frozen_string_literal: true
 
-SyncToNlimsService.push_order_to_nlims if Config.local_nlims?
-SyncToNlimsService.push_order_update_to_nlims
-SyncToNlimsService.push_status_to_nlims
-SyncToNlimsService.push_result_to_nlims
-SyncToNlimsService.push_acknwoledgement_to_master_nlims if Config.local_nlims?
-SyncToNlimsService.synchronize_test_catalog
+begin
+  SyncToNlimsService.synchronize_test_catalog
+rescue StandardError => e
+  puts "Error: #{e.message} ==> Synchronize Test Catalog"
+end
+
+begin
+  SyncToNlimsService.push_order_to_nlims if Config.local_nlims?
+rescue StandardError => e
+  puts "Error: #{e.message} ==> Push Order to Master NLIMS"
+end
+
+begin
+  SyncToNlimsService.push_order_update_to_nlims
+rescue StandardError => e
+  puts "Error: #{e.message} ==> Push Order Update to Master NLIMS"
+end
+
+begin
+  SyncToNlimsService.push_status_to_nlims
+rescue StandardError => e
+  puts "Error: #{e.message} ==> Push Status to Master NLIMS"
+end
+
+begin
+  SyncToNlimsService.push_result_to_nlims
+rescue StandardError => e
+  puts "Error: #{e.message} ==> Push Result to Master NLIMS"
+end
+
+begin
+  SyncToNlimsService.push_acknwoledgement_to_master_nlims if Config.local_nlims?
+rescue StandardError => e
+  puts "Error: #{e.message} ==> Push Acknowledgement to Master NLIMS"
+end
