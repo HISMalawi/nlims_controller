084de2838d7c2305e60495c3b1eac866615ef388
Hopson Gausi <hopgausi20@gmail.com>
2026-06-01 12:07:30 +0200

    Refactor code structure for improved readability and maintainability

--- bin/update_metadata.sh
+++ bin/update_metadata.sh
@@ -10,7 +10,8 @@ rm Gemfile.lock && bundle install --local && bundle exec rails db:migrate && (
   bundle exec rake db:seed:specific\[seed_additional_test_statuses.rb\] &&
   bundle exec rake db:seed:specific\[seed_roles.rb\] &&
   bundle exec rake db:seed:specific\[add_roles_to_users.rb\] &&
-  bundle exec rake db:load_metadata
+  bundle exec rake db:load_metadata &&
+  bundle exec rails r bin/mlab/clear_sync_trackers.rb
 )
 bundle exec rails r bin/updater.rb && bash bin/add_cronjob.sh
 
