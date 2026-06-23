b06fa60a81f2a3f74614d71d8b1970bdff35110b
Hopson Gausi <hopgausi20@gmail.com>
2026-06-23 06:55:04 +0200

    test catalog v16

--- db/sql/metadata/testtype_organisms.sql
+++ db/sql/metadata/testtype_organisms.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: catalog
 -- ------------------------------------------------------
--- Server version	8.0.46-0ubuntu0.24.04.2
+-- Server version	8.0.46-0ubuntu0.24.04.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-23  4:41:44
+-- Dump completed on 2026-06-23  6:54:44
